5d64436d8319001598a650811262eeb0fbc3b7ed
Allon Segall <Segal500@gmail.com>
2016-12-08 11:16:19 +0200

    new V button

--- jasonApp.js
+++ jasonApp.js
@@ -4,7 +4,6 @@ controller('movieController', function($scopw, $http, $log) {
 		method:'GET',
 		url:'https://api.themoviedb.org/3/discover/movie?api_key=51709f2e74017c9f066e0946cb8568e3&with_genres=(GenreID)&with_cast=(ActorID)&sort_by=popularity.desc'});
 		then(function(response){
-			console.log(response);
 			$scope.employees = response.data;
 			$log.info(response);
 		});
